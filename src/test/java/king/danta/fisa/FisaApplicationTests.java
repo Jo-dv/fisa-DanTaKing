88e990e6e1f2ff4031f817d33b44eb5521b4824d
HeeYeon-Ko <rhgmldus6@gmail.com>
2026-04-01 17:09:54 +0900

    init

---
+++ src/test/java/king/danta/fisa/FisaApplicationTests.java
@@ -0,0 +1,13 @@
+package king.danta.fisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
